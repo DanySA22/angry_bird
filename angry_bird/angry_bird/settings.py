ffe69d420d05b619e440fc74ce32603d305d2fbb
DanySA22 <winache22@gmail.com>
2023-09-05 10:16:05 -0700

    adding debuggar app and admin site

--- angry_bird/angry_bird/settings.py
+++ angry_bird/angry_bird/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'debug_toolbar',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -42,14 +43,19 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware'
 ]
+
+INTERNAL_IPS = [
+    '127.0.0.1'
+] 
 
 ROOT_URLCONF = 'angry_bird.urls'
 
--- angry_bird/game/models.py
+++ angry_bird/game/models.py
@@ -6,5 +6,8 @@ class Customer(models.Model):
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
     score = models.IntegerField()
-    profile_image = models.ImageField(upload_to=None, height_field=None, width_field=None, max_length=100)
+    # profile_image = models.ImageField(upload_to=None, height_field=None, width_field=None, max_length=100)
     rating = models.IntegerField()
+    # full_name = ['first_name' + ' ' + 'last_name']
+    def __str__(self) -> str:
+        return self.first_name
