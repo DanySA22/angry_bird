5c1224907ba78026cc565dde5f30d21642f99af6
DanySA22 <winache22@gmail.com>
2023-09-05 08:01:26 -0700

    Working on models

--- angry_bird/game/models.py
+++ angry_bird/game/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Customer(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    score = models.IntegerField()
+    profile_image = models.ImageField(upload_to=None, height_field=None, width_field=None, max_length=100)
+    rating = models.IntegerField()
